97daea8b46530dd3fccd139761380b4481d2cb6a
karmaKiller3352 <alievsul@gmail.com>
2020-07-24 17:52:41 +0300

    deploy

---
+++ precache-manifest.34cac3184b1beafce4a59fc1e8ff9bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1577a14ca793a2cd8f3d8f1cc845c1",
+    "url": "/react-bubble-sort/index.html"
+  },
+  {
+    "revision": "57993412f717735eedf2",
+    "url": "/react-bubble-sort/static/css/main.bd423ce5.chunk.css"
+  },
+  {
+    "revision": "06b829142c306b2eed80",
+    "url": "/react-bubble-sort/static/js/2.338a5c33.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-bubble-sort/static/js/2.338a5c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57993412f717735eedf2",
+    "url": "/react-bubble-sort/static/js/main.82472e45.chunk.js"
+  },
+  {
+    "revision": "4e577a1e76a29b5c34d7",
+    "url": "/react-bubble-sort/static/js/runtime-main.60fccf4e.js"
+  }
+]);
